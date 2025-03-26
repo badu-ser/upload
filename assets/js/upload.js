0e97e45e02b39178d2394204dcfda78ae554bb76
Badu_Ser <100788930+badu-ser@users.noreply.github.com>
2025-03-27 00:28:23 +0530

    Update upload.js

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -12,38 +12,34 @@ function addImg(ele, content) {
         myDIV.appendChild(newContent.firstChild);
     }
 }
-var feedback = function (res) {
-    reportInfo(res, true);
+var feedback = function(res) {
+    var statusDiv = document.querySelector('.upload-status');
+    statusDiv.innerHTML = '';
+    
     if (res.success === true) {
         var get_link = res.data.link.replace(/^http:\/\//i, 'https://');
-        document.querySelector('.status').classList.add('bg-image', 'center', 'mt-2', 'py-2');
-        var content = `<div style="width: 95%;">
-        <ul class="nav nav-tabs" id="myTab" role="tablist">
-            <li class="nav-item">
-                <a class="nav-link active" data-toggle="tab" href="#url" role="tab" aria-controls="url" aria-selected="true">URL</a>
-            </li>
-            
-        </ul>
-        <div class="tab-content" id="myTabContent">
-            <div class="tab-pane fade show active" id="url" role="tabpanel" aria-labelledby="url-tab">
-                <div class="my-2"><input class="form-control" value="${get_link}" onclick="this.select();"/></div>
+        var content = `
+            <div class="alert alert-success mt-3">
+                <input class="form-control mb-2" value="${get_link}" onclick="this.select()">
+                <img src="${get_link}" class="img-fluid rounded">
             </div>
-            <div class="tab-pane fade" id="html" role="tabpanel" aria-labelledby="html-tab">
-                <div class="my-2"><input class="form-control" value="\<img\ src='${get_link}'/>" onclick="this.select();"/></div>
-            </div>
-            <div class="tab-pane fade" id="bbcode" role="tabpanel" aria-labelledby="bbcode-tab">
-                <div class="my-2"><input class="form-control" value="\[img\]${get_link}\[\/img\]" onclick="this.select();"/></div>
-            </div>
-            <div class="tab-pane fade" id="md" role="tabpanel" aria-labelledby="md-tab">
-                <div class="my-2"><input class="form-control" value="![](${get_link})" onclick="this.select();"/></div>
-            </div>
-        </div>
-        <hr><p><img class="img rounded mx-auto d-block" src="${get_link}" alt="Image"/></p>
-        </div>`;
-        addImg('.status', content);
+        `;
+        statusDiv.innerHTML = content;
+    } else {
+        statusDiv.innerHTML = `<div class="alert alert-danger">Upload failed: ${res.data.error}</div>`;
     }
 };
 
+// Initialize with modified options
+var imgurClient = new Imgur({
+    clientid: '146def7f79c7a87',
+    callback: feedback
+});
+
+// Add click handler for the button
+document.querySelector('.upload-button').addEventListener('click', function() {
+    document.getElementById('imgurUpload').click();
+});
 new Imgur({
     clientid: '146def7f79c7a87', //You can change this ClientID
     callback: feedback
