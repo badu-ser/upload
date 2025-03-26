568272e50fa1971f7ad3de766c1164387bee73ea
Badu_Ser <100788930+badu-ser@users.noreply.github.com>
2025-03-27 00:39:47 +0530

    Update upload.js

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -22,28 +22,12 @@ var feedback = function (res) {
             <li class="nav-item">
                 <a class="nav-link active" data-toggle="tab" href="#url" role="tab" aria-controls="url" aria-selected="true">URL</a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" data-toggle="tab" href="#html" role="tab" aria-controls="html" aria-selected="false">HTML</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" data-toggle="tab" href="#bbcode" role="tab" aria-controls="bbcode" aria-selected="false">BBCODE</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" data-toggle="tab" href="#md" role="tab" aria-controls="md" aria-selected="false">MARKDOW</a>
-            </li>
         </ul>
         <div class="tab-content" id="myTabContent">
             <div class="tab-pane fade show active" id="url" role="tabpanel" aria-labelledby="url-tab">
                 <div class="my-2"><input class="form-control" value="${get_link}" onclick="this.select();"/></div>
             </div>
-            <div class="tab-pane fade" id="html" role="tabpanel" aria-labelledby="html-tab">
-                <div class="my-2"><input class="form-control" value="\<img\ src='${get_link}'/>" onclick="this.select();"/></div>
-            </div>
-            <div class="tab-pane fade" id="bbcode" role="tabpanel" aria-labelledby="bbcode-tab">
-                <div class="my-2"><input class="form-control" value="\[img\]${get_link}\[\/img\]" onclick="this.select();"/></div>
-            </div>
-            <div class="tab-pane fade" id="md" role="tabpanel" aria-labelledby="md-tab">
-                <div class="my-2"><input class="form-control" value="![](${get_link})" onclick="this.select();"/></div>
+            
             </div>
         </div>
         <hr><p><img class="img rounded mx-auto d-block" src="${get_link}" alt="Image"/></p>
